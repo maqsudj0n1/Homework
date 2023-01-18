f090adc7ab4db07e3ecee5f46b16ffdd33cb5fcf
maqsudj0n1 <maqsudhaydarov1203@gmail.com>
2023-01-18 14:32:30 +0500

    kiritildi

---
+++ Homework/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Person
+    {
+        public void Person1()
+        {
+            Console.WriteLine("Person");
+        }
+    }
+}
